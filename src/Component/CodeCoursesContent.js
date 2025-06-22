167bcace911f1574472a22fe81d51ca2b3f4d397
Mustafa-Shah444 <mustafashah60430@gmail.com>
2025-06-23 01:53:31 +0530

    Create CodeCoursesContent.js

---
+++ src/Component/CodeCoursesContent.js
@@ -0,0 +1,139 @@
+const Courses = [
+{
+    id: 1,
+    title: "Complete Java Course",
+    price: 15000,
+    description: "Java is an extremely transferable programming language used across platforms and different types of devices, from smartphones to smart TVs.",
+    image : "/courses-images/java-curse-image.jpeg",
+    category: "Web Courses",
+
+},
+{
+    id: 2,
+    title: "Complete Python Course",
+    price: 20000,
+    description: "A Python course is a structured program that teaches individuals how to use the Python programming language.",
+    image : "/courses-images/python-course-image.webp",
+    category: "Web Courses",
+
+},
+{
+    id: 3,
+    title: "Complete Mern Stack Course",
+    price: 35000,
+    description: "A MERN stack course provides training on building full-stack web applications using MongoDB, Express.js, React, and Node.js.",
+    image : "/courses-images/mern-stack-course-image.jpg",
+    category: "Web Courses",
+
+},
+{
+    id: 4,
+    title: "Complete Mean Stack Course",
+    price: 30000,
+    description: "The MEAN stack is a JavaScript-based framework for developing scalable web applications.",
+    image : "/courses-images/mean-stack-course-image.jpg",
+    category: "Web Courses",
+
+},
+{
+    id: 5,
+    title: "Complete Full Stack Course",
+    price: 15000,
+    description: "A full-stack developer course equips individuals with the skills to build both the front-end and back-end of web applications.",
+    image : "/courses-images/full-stack-image.png",
+    category: "Web Courses",
+
+},
+{
+    id: 6,
+    title: "Complete C++ Course",
+    price: 15000,
+    description: "A C++ course typically covers the fundamentals of the C++ programming language, including syntax, data types, control structures, and object-oriented programming concepts.",
+    image : "/courses-images/c++-course-image.jpeg",
+    category: "Web Courses",
+
+},
+{
+    id: 7,
+    title: "Complete SEO Course",
+    price: 14000,
+    description: "An SEO (Search Engine Optimization) course typically covers the principles and techniques for improving a website's visibility in search engine results.",
+    image : "/courses-images/seo-courses-image.jpeg",
+    category: "Marketing Courses",
+
+},
+{
+    id: 8,
+    title: "Complete Google Ads Course",
+    price: 20000,
+    description: "Advertising courses equip individuals with the skills to develop, implement, and manage advertising campaigns across various platforms.",
+    image : "/courses-images/ads-course-image.png",
+    category: "Marketing Courses",
+
+},
+{
+    id: 9,
+    title: "Complete Content Marketing Course",
+    price: 10000,
+    description: "A content marketing course provides training on how to create, distribute, and manage valuable and relevant content to attract and engage a target audience.",
+    image : "/courses-images/mern-stack-course-image.jpg",
+    category: "Marketing Courses",
+
+},
+{
+    id: 10,
+    title: "Complete E-mail Marketing Course",
+    price: 10000,
+    description: "email marketing can be a good career path. The industry is growing, with many businesses recognizing the effectiveness of email as a direct marketing channel.",
+    image : "/courses-images/e-mail-marketing-image.jpeg",
+    category: "Marketing Courses",
+
+},
+{
+    id: 11,
+    title: "Complete Social Media Marketing Course",
+    price: 37000,
+    description: "Social media marketing courses teach strategies for promoting products or services through social media platforms.",
+    image : "/courses-images/social-media-course-image.jpeg",
+    category: "Marketing Courses",
+
+},
+{
+    id: 12,
+    title: "Complete Investment Managment Course",
+    price: 10000,
+    description: "Investment management refers to the handling of financial assets and other investments by professionals for clients.",
+    image : "/courses-images/investement-management-course-image.avif",
+    category: "Tradding Courses",
+
+},
+{
+    id: 13,
+    title: "Complete Tradding Basics Course",
+    price: 25000,
+    description: "Trading means buying and selling monetary assets like shares, bonds, commodities, currencies, and derivatives in numerous markets.",
+    image : "/courses-images/tradding-basics-image.jpeg",
+    category: "Tradding Courses",
+
+},
+{
+    id: 14,
+    title: "Complete Fundamental Analysis Course",
+    price: 32000,
+    description: "Fundamental analysis is a method of evaluating the intrinsic value of an asset and analysing the factors that could influence its price in the future.",
+    image : "/courses-images/fundamental-analysis-image.jpeg",
+    category: "Tradding Courses",
+
+},
+{
+    id: 15,
+    title: "Complete Tradding Algorithm Course",
+    price: 21000,
+    description: " execution algorithms, profit-seeking or black-box algorithms, and high-frequency trading (HFT) algorithms.",
+    image : "/courses-images/tradding-algothrithm-course-image.jpg",
+    category: "Tradding cart",
+
+}
+]
+
+export default Courses;
